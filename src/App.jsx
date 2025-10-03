0b12114338ced563294f3ed1b47da7d87837a5bb
xihubet1-ctrl <xihubet1@gmail.com>
2025-10-03 15:42:53 +0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TypingTest from "./components/TypingTest.jsx";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <TypingTest />
+    </div>
+  );
+}
+
+export default App;
